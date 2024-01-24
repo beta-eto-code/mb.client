b8355f9f007dd19d69d3a2466b357557cdf65558
Nesterov Alexandr <alex19pov31@gmail.com>
2024-01-24 17:19:24 +0300

    feat(consumer): update consumer interface - add iterator method

--- src/ConsumerInterface.php
+++ src/ConsumerInterface.php
@@ -2,7 +2,15 @@
 
 namespace MessageBroker;
 
+use Iterator;
+
 interface ConsumerInterface
 {
+    /**
+     * @param string $exchangeKey
+     * @param array $options
+     * @return Iterator|MessageInterface[]
+     */
+    public function getMessageIterator(string $exchangeKey, array $options = []): Iterator;
     public function getMessage(string $exchangeKey, array $options = []): ?MessageInterface;
 }
